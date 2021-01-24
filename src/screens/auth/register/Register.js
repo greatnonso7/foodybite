069d4572e710c36858a7afe92fecfb7c2a0bd08f
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-01-24 11:15:32 +0100

    Register Screen done

--- src/screens/auth/register/Register.js
+++ src/screens/auth/register/Register.js
@@ -7,9 +7,11 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 import { Button, StyleGuide } from 'components';
 import Input from 'components/Forms/Input';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Register = ({ navigation }) => {
   return (
@@ -21,6 +23,9 @@ const Register = ({ navigation }) => {
         <View style={styles.overlay} />
         <SafeAreaView
           style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <View style={styles.iconContainer}>
+            <Ionicons name="person-outline" size={50} color="black" />
+          </View>
           <View style={styles.formContainer}>
             <Input placeholder="Name" iconName="person-outline" />
             <Input placeholder="Email" iconName="mail-outline" />
@@ -35,7 +40,7 @@ const Register = ({ navigation }) => {
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.textAccount}>Already have an account? </Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
               <Text style={styles.textNavigate}>Login</Text>
             </TouchableOpacity>
           </View>
@@ -57,6 +62,23 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     opacity: 0.8,
     backgroundColor: 'black',
+  },
+  iconContainer: {
+    position: 'relative',
+    top: StyleGuide.spacing * 8,
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    opacity: 0.5,
+  },
+  formContainer: {
+    marginTop:
+      Platform.OS === 'android'
+        ? StyleGuide.spacing * 12
+        : StyleGuide.spacing * 18,
   },
   textContainer: {
     flexDirection: 'row',
